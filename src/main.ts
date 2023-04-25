6b05c5b06fe95c1ab5af5658f1a0de13bb60d54e
Brad Butler <bradbutler75@gmail.com>
2023-04-25 17:09:00 -0300

    Added zone import to avoid stackblitz error.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'zone.js/dist/zone'; // Avoid error in StackBlitz
 
 import { AppModule } from './app/app.module';
 
